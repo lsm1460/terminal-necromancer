1146a520271405a4f16972c1cbf1d3b628b5a7f1
lsm <lsm1460@gmail.com>
2026-04-30 14:04:48 +0900

    feat: alone  상향

--- src/systems/job/necromancer/Modifiers.ts
+++ src/systems/job/necromancer/Modifiers.ts
@@ -1,31 +1,42 @@
-import { StatModifier } from '~/core/player/StatsCalculator';
-import { Necromancer } from './Necromancer';
+import { StatModifier } from '~/core/player/StatsCalculator'
+import { Necromancer } from './Necromancer'
 
 export const necromancerModifiers: StatModifier[] = [
   {
     key: 'maxHp',
     value: (current, p) => {
       if (p.hasAffix('BLOOD')) {
-        const potentialMp = p._maxMp + (p.equipped.weapon?.mp || 0) + (p.equipped.armor?.mp || 0);
-        return Math.floor((current + potentialMp) * 1.3);
+        const potentialMp = p._maxMp + (p.equipped.weapon?.mp || 0) + (p.equipped.armor?.mp || 0)
+        return Math.floor((current + potentialMp) * 1.3)
       }
-      return current;
-    }
+      return current
+    },
   },
-  
+
   {
     key: 'maxMp',
-    value: (current, p) => (p.hasAffix('BLOOD') ? 0 : current)
+    value: (current, p) => (p.hasAffix('BLOOD') ? 0 : current),
   },
 
   {
     key: 'atk',
     value: (current, p) => {
-      const n = p as Necromancer; // 타입 캐스팅
+      const n = p as Necromancer // 타입 캐스팅
       if (p.hasAffix('ALONE') && n.minionManager.skeleton.length < 1) {
-        return current + (n.minionManager.maxSkeleton * 10);
+        return current + n.minionManager.maxSkeleton * 15
       }
-      return current;
-    }
-  }
-];
+      return current
+    },
+  },
+
+  {
+    key: 'def',
+    value: (current, p) => {
+      const n = p as Necromancer // 타입 캐스팅
+      if (p.hasAffix('ALONE') && n.minionManager.skeleton.length < 1) {
+        return current + n.minionManager.maxSkeleton * 10
+      }
+      return current
+    },
+  },
+]
